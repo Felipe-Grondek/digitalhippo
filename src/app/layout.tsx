d68cb6d263ea7b82c0da832a9c39806607fb7f17
Felipe Grondek <felipegrondek00@gmail.com>
2023-11-25 00:09:01 -0300

    add nav bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
+import NavBar from '@/components/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,12 +23,13 @@ export default function RootLayout({
           "relative h-full font-sans antialiased",
           inter.className
         )}>
-          <main className='relative flex flex-col min-h-screen'>
-            <div className='flex-grow flex-1'>
-              {children}
-            </div>
-          </main>
-        </body>
+        <main className='relative flex flex-col min-h-screen'>
+          <NavBar />
+          <div className='flex-grow flex-1'>
+            {children}
+          </div>
+        </main>
+      </body>
     </html>
   )
 }
